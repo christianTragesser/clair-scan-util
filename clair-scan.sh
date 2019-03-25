7d5bcaa86d299505175f77af75fb1a12fb5a204c
Christian Tragesser <christian@evoen.net>
2019-03-24 22:18:00 -0500

    change name of clair backed host

--- clair-scan.sh
+++ clair-scan.sh
@@ -4,4 +4,4 @@ HOST_IP=$(hostname -i)
 IMAGE=${1//\//-}
 TIMESTAMP=$(date '+%m-%d-%Y-%Hh%Mm%Ss')
 
-clair-scanner --ip $HOST_IP -c http://clair:6060 -r /tmp/${IMAGE}.${TIMESTAMP}.cve-scan.json $1
+clair-scanner --ip $HOST_IP -c http://clair-backend:6060 -r /tmp/${IMAGE}.${TIMESTAMP}.cve-scan.json $1
